f3424a493e3e12cc3b76fefac376c93ca400b36b
Pc <kevsertemur@gmail.com>
2022-12-14 23:16:35 +0300

    day01

---
+++ src/test/java/Day1_SeleniumMaven/C03_JunitAssertions.java
@@ -0,0 +1,34 @@
+package Day1_SeleniumMaven;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+
+public class C03_JunitAssertions {
+
+    //    Go to Amazon homepage.
+    //    Do the following tasks by creating 3 different test methods.
+    //    1- Test if the URL contains Amazon.
+    //    2- Test if the title does not contain Facebook.
+    //    3- Test that the Amazon logo appears in the upper left corner.
+
+    WebDriver driver;
+
+    @Before
+    public void before(){
+
+    }
+
+    @After
+    public void after(){
+
+    }
+
+    @Test
+    public void assertionTest(){
+
+    }
+
+
+}
