9bc5b591584211804ee6b47578a02cb59a062f13
Pc <kevsertemur@gmail.com>
2022-12-20 21:21:08 +0300

    day06

---
+++ src/test/java/Day6_DropDown_SeleniumWaits/C01_Faker.java
@@ -0,0 +1,14 @@
+package Day6_DropDown_SeleniumWaits;
+
+import org.junit.Test;
+
+public class C01_Faker {
+
+
+    @Test
+    public void fakerExample(){
+        Faker faker = new Faker();
+        faker.name().firs
+
+    }
+}
