253a01e0684d8ea2ad9d764032571f7e186f438e
Pc <kevsertemur@gmail.com>
2022-12-21 20:45:28 +0300

    day06

---
+++ src/test/java/Day6_DropDown_SeleniumWaits/homework_select.java
@@ -0,0 +1,16 @@
+package Day6_DropDown_SeleniumWaits;
+
+public class homework_select {
+
+    /*
+    Launch the browser.
+            Open "https://demoqa.com/select-menu".
+    Select the Standard Multi-Select using the element id.
+    Verifying that the element is multi-select.
+            Select 'Opel' using the index and deselect the same using index.
+            Select 'Saab' using value and deselect the same using value.
+    Deselect all the options.
+    Close the browser.
+     */
+
+}
