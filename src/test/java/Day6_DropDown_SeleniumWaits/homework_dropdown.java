0da19960143dfb825aae587fa321ba93409e1b13
Pc <kevsertemur@gmail.com>
2022-12-21 20:45:16 +0300

    day06

---
+++ src/test/java/Day6_DropDown_SeleniumWaits/homework_dropdown.java
@@ -0,0 +1,38 @@
+package Day6_DropDown_SeleniumWaits;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class homework_dropdown {
+
+    /*
+    Go to URL: https://the-internet.herokuapp.com/dropdown
+    Create method selectByIndexTest and Select Option 1 using index .
+    Create method selectByValueTest Select Option 2 by value.
+    Create method selectByVisibleTextTest Select Option 1 value by visible text.
+    Create method printAllTest Print all dropdown value.
+    Verify the dropdown has Option 2 text.
+    Create method printFirstSelectedOptionTest Print first selected option.
+    Find the size of the dropdown, Print "Expected Is Not Equal Actual" if there are not 3 elements in the dropdown.
+    */
+
+    WebDriver driver;
+
+    @Before
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+}
