17dc0f623ffba3f55b6dd1fb0b729f2799c96649
Pc <kevsertemur@gmail.com>
2022-12-23 20:49:07 +0300

    day09

--- src/test/java/mentoring/Homework_22Aralık.java
+++ src/test/java/mentoring/Homework_22Aralık.java
@@ -2,5 +2,12 @@ package mentoring;
 
 public class Homework_22Aralık {
 
+/*
+1. https://www.amazon.com/ adresine gidin.
+2.  Assertion kullanarak Amazon web sitesine gittiğinizi doğrulayın.
+3. Kategori DropDown'dan Books kategorisini seçin ve arama kutusuna "Selenium" yazdırıp aratın.
+4. Çıkan kitaplardan 2. ve 5. kitabın isminde "Selenium" kelimesinin geçtiğini doğrulayın.
+5. Çıkan sonuç sayısının 500'den büyük olduğunu doğrulayın.
+ */
 
 }
